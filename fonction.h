bf303338f10f9c4239483fc7e3368f8a2eb1b4e1
PETIT ANGUS p2301811 <angus.petit@etu.univ-lyon1.fr>
2023-12-14 16:49:09 +0000

    Replace fonction.h

--- fonction.h
+++ fonction.h
@@ -1,6 +1,14 @@
+#ifndef SAE_C_FINAL_FONCTION_H
+#define SAE_C_FINAL_FONCTION_H
+
+#endif //SAE_C_FINAL_FONCTION_H
+
 // Déclarations de nos fonctions
 
 void affichage_general();
 void afficherVolsJournee();
-void rechercherVol();
+void rechercherVolCompany();
 void afficherPassagersSalleEmbarquement();
+void choix_recherche_vol();
+void rechercherVolDestination();
+void rechercherVolHorraire();
