6722b812ec30ecc67758e9c068ed32fc0acf2fd1
PETIT ANGUS p2301811 <angus.petit@etu.univ-lyon1.fr>
2024-01-03 18:54:43 +0000

    Replace fonction.h

--- fonction.h
+++ fonction.h
@@ -3,12 +3,31 @@
 
 #endif //SAE_C_FINAL_FONCTION_H
 
+
+struct Vol {
+    char numero_vol[1000];
+    char compagnie[1000];
+    char destination[1000];
+    char numero_comptoir[1000];
+    char heure_debut_enregistrement[1000];  //int
+    char heure_fin_enregistrement[1000];  //int
+    char salle_embarquement[1000];
+    char heure_debut_embarquement[1000];   //int
+    char heure_fin_embarquement[1000];  //int
+    char heure_decollage[1000];    //int
+    char etat[1000];    //int
+};
+
 // Déclarations de nos fonctions
 
 void affichage_general();
 void afficherVolsJournee();
-void rechercherVolCompany();
+void rechercherVolCompany(struct Vol tab[], int taille_tab);
 void afficherPassagersSalleEmbarquement();
 void choix_recherche_vol();
 void rechercherVolDestination();
 void rechercherVolHorraire();
+void rechercheVol2();
+void chemin_access();
+
+
