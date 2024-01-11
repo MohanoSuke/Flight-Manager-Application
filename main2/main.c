9ed19485885537e248f79d3c0f675970a63792dd
PETIT ANGUS p2301811 <angus.petit@etu.univ-lyon1.fr>
2024-01-11 09:23:20 +0000

    Replace main.c

--- main2/main.c
+++ main2/main.c
@@ -1,9 +1,21 @@
-#include "fonction.c"
+#include "fonction.h"
 
 
 int main() {
 
+    printf("       ______\n");
+    printf("  _\\ _~-\\___\n");
+    printf(" =  = ==(____AA____D\n");
+    printf("           \\_____\\___________________,-~~~~~~~`-..\n");
+    printf("           /     o O o o o o O O o o o o o o O o  |\\_\n");
+    printf("           `~-.__        ___..----..                  )\n");
+    printf("                 `---~~\\___________/------------`````\n");
+    printf("                 =  ===(_________D\n");
+    printf("\n");
     chemin_access();
+
+    printf("\n");
     affichage_general();
     return 0;
+
 }
